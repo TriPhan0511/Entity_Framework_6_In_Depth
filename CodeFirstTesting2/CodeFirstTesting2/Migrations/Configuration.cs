9534730eb609f18a634d31f1a7f30f8c3398c782
Tri Phan <phanphutri@gmail.com>
2022-05-25 09:47:05 +0700

    Updated

---
+++ CodeFirstTesting2/CodeFirstTesting2/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirstTesting2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstTesting2.PlutoModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirstTesting2.PlutoModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
