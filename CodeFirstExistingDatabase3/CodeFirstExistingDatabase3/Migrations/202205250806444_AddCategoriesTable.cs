4ba05fc231211d27d54a072f8f154e3232d3c4ee
Tri Phan <phanphutri@gmail.com>
2022-05-25 15:52:34 +0700

    Updated

---
+++ CodeFirstExistingDatabase3/CodeFirstExistingDatabase3/Migrations/202205250806444_AddCategoriesTable.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeFirstExistingDatabase3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            // Use DbMigration.Sql() method to execute a SQL command or set of SQL commands
+            Sql("INSERT INTO Categories VALUES (1, 'Web Development')");
+            Sql("INSERT INTO Categories VALUES (2, 'Programming Languages')");
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
