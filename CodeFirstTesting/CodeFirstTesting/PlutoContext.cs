9458e57486ca5f03650a1c450ed0eec0fe1d2876
Tri Phan <phanphutri@gmail.com>
2022-05-25 07:41:55 +0700

    Updated

---
+++ CodeFirstTesting/CodeFirstTesting/PlutoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+
+namespace CodeFirstTesting
+{
+    internal class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            :base("name=PlutoConnectionString")
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
